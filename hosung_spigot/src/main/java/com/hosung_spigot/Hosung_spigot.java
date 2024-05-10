3b6431cc1d7d93cbce5bb25f758011a3279e125c
Hosung <an3735297@naver.com>
2024-05-10 14:06:40 +0900

    프로젝트 생성

---
+++ hosung_spigot/src/main/java/com/hosung_spigot/Hosung_spigot.java
@@ -0,0 +1,17 @@
+package com.hosung_spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hosung_spigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
