e29074a6c8d708b7ab0fd3a3f8c3b9357e2e77f4
Hosung <an3735297@naver.com>
2024-05-10 15:42:23 +0900

    앤티티 타고 내리기

--- hosung_spigot/src/main/java/com/hosung_spigot/Hosung_spigot.java
+++ hosung_spigot/src/main/java/com/hosung_spigot/Hosung_spigot.java
@@ -7,6 +7,7 @@ public final class Hosung_spigot extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new RiddingEntityListener(), this);
         getServer().getPluginManager().registerEvents(new GunListener(), this);
         getCommand("creeper").setExecutor(new CreeperCommand());
         getCommand("sword").setExecutor(new SwordCommand());
