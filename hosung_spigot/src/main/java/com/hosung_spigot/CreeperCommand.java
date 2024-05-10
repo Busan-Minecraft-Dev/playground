afd5d56641ea152109b5e9be713a128897f041a5
Hosung <an3735297@naver.com>
2024-05-10 14:32:01 +0900

    크리퍼 위치가 랜덤하게 나타나도록 변경하였습니다.

--- hosung_spigot/src/main/java/com/hosung_spigot/CreeperCommand.java
+++ hosung_spigot/src/main/java/com/hosung_spigot/CreeperCommand.java
@@ -1,18 +1,25 @@
 package com.hosung_spigot;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
+
 public class CreeperCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            Bukkit.getWorld("world").spawnEntity(player.getLocation(), EntityType.CREEPER);
+            Location location = player.getLocation();
+            Random random = new Random();
+            location.setX(location.getX() + random.nextInt(10) - 5);
+            location.setZ(location.getZ() + random.nextInt(10) - 5);
+            Bukkit.getWorld("world").spawnEntity(location, EntityType.CREEPER);
             return true;
         }
         return false;
